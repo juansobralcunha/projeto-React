41cfa45d746611b8251687448e92331145146dd2
juan sobral cunha <sobral800@hotmail.com>
2022-06-03 09:45:10 -0300

    REACT-BASE

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#A6040A';
+export const primaryDarkColor = '#1A1A1D';
+
+export const succesColor = '#0197F6';
+export const infoColor = '#0197F6';
+export const errorColor = '#F2AF29';
+export const warningColor = '#F2AF29';
